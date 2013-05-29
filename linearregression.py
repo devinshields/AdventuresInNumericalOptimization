ede0ac9e716c4c4b2639d04ea23583b4c07a05d3
Devin Shields <devin.shields@gmail.com>
2013-05-29 17:53:45 -0400

    gradient decent for least squares is working, runs pretty fast on a small data set with low condition number

--- linearregression.py
+++ linearregression.py
@@ -1,78 +1,103 @@
 #!/usr/bin/python
+''' this module solves an example of the least squares regression problem 
+'''
+
 
 import numpy as np
-import logging
 
+def least_squares_via_gradient_descent(A, b, max_gradient_norm=10**-6):
+  ''' http://en.wikipedia.org/wiki/Gradient_descent#Solution_of_a_linear_system
+        Minimize: ||A*x - b||
+        Where   : A is a known, N-by-N matrix
+                : b is a known, N vector
+                : x is an unknown N vector'''
 
-def setup_logger():
-  ''' console + file logger to track experimental results '''
-  logger    = logging.getLogger('adventure_log')
-  logger.setLevel(logging.INFO)
-  fh        = logging.FileHandler('adventure.log')
-  fh.setLevel(logging.INFO)
-  ch        = logging.StreamHandler()
-  ch.setLevel(logging.INFO)
-  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-  fh.setFormatter(formatter)
-  ch.setFormatter(formatter)
-  logger.addHandler(fh)
-  logger.addHandler(ch)
-  return logger
-
-logger = setup_logger()
-
-
-def get_linear_simulation_data(model_params=[[1],[2]], variance=5, n=50, seed=42):
-  ''' builds 2D simulation data for fitting experiments'''
-  np.random.seed(seed=seed)
-  e = np.matrix(np.random.randn(n)).T * variance
+  # check that A is a square matrix, b size matches A, and that the system is over-determined
+  assert isinstance(A.shape[0], (int, long)) and isinstance(A.shape[1], (int, long))
+  assert len(A.shape) == 2
+  assert A.shape[0] == b.shape[0]
+  assert b.shape[1] == 1
+  assert A.shape[0] >= A.shape[1]
   
-  x = np.matrix(model_params)
-  A = np.matrix([np.ones(n), np.arange(n)]).T
-  b = A*x + e
-  return A, b
+  def f(x, A=A, b=b):
+    ''' measure of the optimization value |A*x - b| at point x '''
+    return np.linalg.norm(A*x-b)
 
-
-def solve_gradient_decent(A, b, x_guess=np.array([[0], [0]]), step_size=.00001, max_gradient_norm=.01):
-  ''' http://en.wikipedia.org/wiki/Gradient_descent#Solution_of_a_linear_system '''
-
-  def get_gradient(A, b, x):
+  def f_grad(x, A=A, b=b):
+    ''' gradient of the function |A*x - b| measured at point x'''
     return 2*A.T*(A*x - b)
 
+  def line_search(x, x_delta, t=1, f=f, f_grad=f_grad, beta=.9):
+    ''' define a back-tracking line search tool. 'x_delta' represents search direction. '''
+    while f(x + t * x_delta) > f(x + beta * t * x_delta):
+      t = beta * t
+    return beta * t
 
-  grad = get_gradient(A, b, x_guess)
-  while np.linalg.norm(grad) > max_gradient_norm:
+  # start at at x = 0, any guess will do
+  x_guess = np.matrix(np.zeros(A.shape[1])).T
 
-    # log the current state of the optimization
-    logger.debug('Optimization Value: {0}'.format(np.linalg.norm(A*x_guess-b)))
-    logger.debug('Guess:', x_guess.T)
-    logger.debug('Gradient:', grad.T)
-    logger.debug('Gradient Norm:', np.linalg.norm(grad))
-
-    # get the gradient at the current guess
-    grad = get_gradient(A, b, x_guess)
-    
-    # calculate a gradient decent step & update the guess
-    x_guess = x_guess - step_size*grad
+  # iterate until optimality
+  while np.linalg.norm(f_grad(x_guess)) > max_gradient_norm:
+    ''' see Boyde, convex optimization, page 464. this line search uses a
+        very simple greedy gradient descent'''
+    x_delta  = -1 * f_grad(x_guess)
+    t        = line_search(x_guess, x_delta)
+    x_guess += t * x_delta
   return x_guess
 
 
-def main ():
-  ''' builds some noisy, 2D linear sample data, then fits linear  '''
 
-  # get some sample data
-  A, b = get_linear_simulation_data()
-  logger.info('Building simulation data: complete')
+# define test data
+np.random.seed(seed=42)
 
-  # solve via the built in linear regressor
-  solution = np.linalg.lstsq(A, np.asarray(b.T)[0])[0].T
-  logger.info('Built-In Solver Solution: {0}'.format(solution))
+n = 5
+A = np.matrix([np.ones(n),range(n)]).T
+x = np.matrix([5, 10]).T
+b = A*x + np.matrix(np.random.normal(size=n)).T
 
-  # solve via a hand-rolled gradient decent function
-  solution = solve_gradient_decent(A, b)
-  logger.info('Homemade Gradient Decent: {0}'.format(solution.T))
 
-  pass
+# call the optimization, print the results, compare with a real solution
+x_opt = least_squares_via_gradient_descent(A, b)
+
+print 'Homegrown Gradient Descent (GD) Method'
+print 'GD - Best Fit Params:'
+print x_opt
+print
+print 'GD - Optimal Value for |A*x - b|:'
+print np.linalg.norm(A*x_opt-b)
+print
+print 'GD - Gradient at Solution:'
+print 2*A.T*(A*x_opt - b)
+print
+print 'GD - Gradient Norm at Solution:'
+print np.linalg.norm(2*A.T*(A*x_opt - b))
+print 
+print
+
+
+# the real solution found the real way: (pseudo inverse of A) * b 
+x_opt = np.linalg.lstsq(A,b)[0]
+
+print 'Real Solution, found via linear algebra packages'
+print 'Real - Best Fit Params:'
+print x_opt
+print
+print 'Real - Optimal Value for |A*x - b|:'
+print np.linalg.norm(A*x_opt-b)
+print
+print 'Real - Gradient at Solution:'
+print 2*A.T*(A*x_opt - b)
+print
+print 'Real - Gradient Norm at Solution:'
+print np.linalg.norm(2*A.T*(A*x_opt - b))
+print 
+print
+print 'Actual solution (parameters used to generate the data):'
+print x
+
+
+def main():
+  '''  '''
 
 if __name__ == '__main__':
   main()
